7785715f33f612140a9eb48be876175fe116a2d6
RedSmileTV <lelledefeo@gmail.com>
2024-12-02 18:09:07 +0100

    Day01 part 1 solved

--- src/day01.rs
+++ src/day01.rs
@@ -1,5 +1,50 @@
+use crate::read_file;
+
 pub fn solve() {
+    let input: Vec<i32> = read_file("inputs/day01_part1.txt");
+    let mut left = split_input(input.clone()).0;
+    let mut right = split_input(input.clone()).1;
 
+    left = sort_ascending(left);
+    right = sort_ascending(right);
 
-    println!("Hello, world!");
+    let differences = get_differences(left, right);
+
+    println!("Day 01 solution: {}", sum(differences));
+}
+
+fn get_differences(left: Vec<i32>, right: Vec<i32>) -> Vec<i32> {
+    let mut differences: Vec<i32> = Vec::new();
+    for i in 0..left.len() {
+        let diff = (right[i] - left[i]).abs();
+        differences.push(diff);
+    }
+    differences
+}
+
+fn sum(input: Vec<i32>) -> i32 {
+    let mut sum = 0;
+    for i in 0..input.len() {
+        sum += input[i];
+    }
+    sum
+}
+
+fn sort_ascending(input: Vec<i32>) -> Vec<i32> {
+    let mut sorted = input.clone();
+    sorted.sort();
+    sorted
+}
+
+fn split_input(input: Vec<i32>) -> (Vec<i32>, Vec<i32>) {
+    let mut first = Vec::new();
+    let mut second = Vec::new();
+    for i in 0..input.len() {
+        if i % 2 == 0 {
+            first.push(input[i]);
+        } else {
+            second.push(input[i]);
+        }
+    }
+    (first, second)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use std::fs::File;
+use std::io::Read;
+
 mod day01;
 
 fn main() {
-
     day01::solve();
+}
+
+pub fn read_file(path: &str) -> Vec<i32> {
+    let mut file = File::open(path).expect("File not found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Error reading file");
+    contents.split_whitespace().map(|s| s.parse().unwrap()).collect()
 }
